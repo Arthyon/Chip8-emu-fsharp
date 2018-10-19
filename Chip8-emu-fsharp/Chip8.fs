9529bb3f29ac33705f352c6dbc2414aa757f0cb5
Christian Hochlin <christian.hochlin@makingwaves.com>
2018-10-19 10:50:12 +0200

    Started on ui

---
+++ Chip8-emu-fsharp/Chip8.fs
@@ -0,0 +1,75 @@
+﻿module Chip8
+
+open System
+
+type Memory = array<uint8>
+type Opcode = uint16
+type FrameType = Drawable | Computational
+
+// Memory layout
+// 0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
+// 0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F)
+// 0x200-0xFFF - Program ROM and work RAM
+type State = { 
+    Memory: Memory;  // 4096
+    V: array<uint8>; // 16
+    pc: uint16;
+    I: uint16;
+    gfx: array<bool>; // 2048
+    delayTimer:uint8;
+    soundTimer: uint8;
+    stack: array<uint16>; // 16
+    sp: uint16;
+    terminating: bool * string;
+}
+
+type Command =
+| SetIndex of uint16
+//| ReturnFromSubroutine
+| ClearScreen
+| Unknown of uint16
+
+
+type Frame = State * FrameType
+
+type KeyInput =
+| NormalPlay of uint8 []
+| Pause
+| Rewind
+| Exit
+
+let FetchFromMemory state (address: uint16 ) =
+    state.Memory.[int32(address)]
+    
+let FetchOpcode state :Opcode =
+    let leftPart = uint16(FetchFromMemory state state.pc)
+    let rightPart = uint16(FetchFromMemory state (state.pc + 1us))
+    (leftPart <<< 8) ||| rightPart
+
+let DecodeNestedOpCode (opcode: Opcode) =
+    match opcode &&& 0x00FFus with
+    | 0x00E0us  -> ClearScreen
+    //| 0x00EEus  -> ReturnFromSubroutine
+    | _         -> Unknown opcode
+
+let DecodeOpCode (opcode: Opcode) =
+    match opcode &&& 0xF000us with
+    | 0xA000us -> SetIndex (opcode &&& 0x0FFFus)
+    | _        -> DecodeNestedOpCode opcode
+
+let ExecuteCommand state command =
+    match command with
+    | SetIndex idx      -> { state with I = idx ; pc = state.pc + 2us }, Computational
+    | Unknown opcode    -> { state with terminating = true, sprintf "Terminating because of unknown opcode %X" opcode }, Computational
+    | ClearScreen       -> { state with gfx = (Array.create 2048 false) }, Drawable
+
+let UpdateTimers frame =
+    let state, frameType = frame
+    { state with delayTimer = Math.Max(0uy, state.delayTimer - 1uy) ; soundTimer = Math.Max(0uy, state.soundTimer - 1uy) }, frameType    
+
+let EmulateCycle state keys =
+    FetchOpcode state
+        |> DecodeOpCode
+        |> ExecuteCommand state
+        |> UpdateTimers
+
--- Chip8-emu-fsharp/Program.fs
+++ Chip8-emu-fsharp/Program.fs
@@ -1,85 +1,52 @@
-﻿
+﻿open Chip8
+open Initialization
+open System.IO
 open System
-type Memory = array<uint8>
-type State = { 
-    Memory: Memory; 
-    V: array<uint8>;
-    pc: uint16;
-    I: uint16;
-    gfx: array<bool>;
-    delayTimer:uint8;
-    soundTimer: uint8;
-    stack: array<uint16>;
-    sp: uint16;
-}
-
-type Command =
-| SetIndex of uint16
-
-type Opcode = uint16
-
-type Frame =
-| DrawableFrame of State
-| ComputationalFrame of State
 
 
-// 0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
-// 0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F)
-// 0x200-0xFFF - Program ROM and work RAM
-let fontset = Array.create 80 0uy
+let TryPlaySound state =
+    if state.soundTimer = 1uy 
+    then printfn "Beep"
 
-let Initialize (programBytes : byte[])=
-    let systemMemory = Array.create 4096 0uy
-    for i = 0 to 79 do
-        systemMemory.[i + 80] <- fontset.[i]
-    for i = 0 to (programBytes.Length - 1) do
-        systemMemory.[i + 512] <- programBytes.[i]
+let Draw state =
+    ()
 
-    {
-        Memory = systemMemory
-        V = (Array.create 16 0uy);
-        pc = 0x200us;
-        I = 0us;
-        gfx = (Array.create 2048 false);
-        delayTimer = 0uy;
-        soundTimer = 0uy;
-        stack = (Array.create 16 0us);
-        sp = 0us;
-    }
+let GetCurrentInput previousInput =
+    previousInput
 
-let FetchFromMemory state (address: uint16 ) =
-    state.Memory.[int32(address)]
-    
-let FetchOpcode state :Opcode =
-    let leftPart = uint16(FetchFromMemory state state.pc)
-    let rightPart = uint16(FetchFromMemory state (state.pc + 1us))
-    (leftPart <<< 8) ||| rightPart
+let NextFrame state keys =
+    let newState, frameType = EmulateCycle state keys
+    match frameType with
+    | Computational -> TryPlaySound newState
+                       newState
+    | Drawable      -> TryPlaySound newState
+                       Draw newState
+                       newState
 
-let DecodeOpCode (opcode: Opcode )=
-    match opcode &&& 0xF000us with
-    | 0xA000us -> SetIndex (opcode &&& 0x0FFFus)
-    | _ -> failwith "Unknown opcode"
-
-let ExecuteCommand command state =
-    match command with
-    | SetIndex idx -> { state with I = idx ; pc = state.pc + 2us }
-
-let EmulateCycle state =
-    let opcode = FetchOpcode (state)
-    let command = DecodeOpCode opcode
-    let state = ExecuteCommand command state
-    { state with delayTimer = Math.Max(0uy, state.delayTimer - 1uy) ; soundTimer = Math.Max(0uy, state.soundTimer - 1uy) }
-
-let PlaySound () =
-    printfn "Beep"
-
+let rec RunGameLoop previousInput (previousStates: State list) state =
+    if fst state.terminating 
+    then state, previousStates
+    else
+        let input = GetCurrentInput previousInput
+        match input with
+        | NormalPlay keys   ->  NextFrame state keys
+                                |> RunGameLoop input (state::previousStates)
+        | Pause             ->  RunGameLoop input previousStates state
+        | Rewind            ->  match previousStates with
+                                | head::tail    ->  RunGameLoop input tail head
+                                | []            ->  RunGameLoop input previousStates state
+        | Exit              ->  state, previousStates
+                
 [<EntryPoint>]
 let main argv =
-    let game = [|0xAFuy ; 0xF2uy|]
+    if argv.Length = 0 && not (File.Exists argv.[0]) then failwith "Invalid path"
 
-    let state = Initialize game
-    let newState = EmulateCycle state
-    if newState.soundTimer = 1uy then PlaySound ()
+    let game = File.ReadAllBytes argv.[0]
+    let initialState = Initialization.Initialize game
 
-    printfn "%X" newState.I
+    let endstate, _ = initialState |> RunGameLoop Initialization.initialInput []
+
+    let _, terminationReason = endstate.terminating
+    printf "%s" terminationReason
+    printf "Last state of I: %X" endstate.I
     0 
