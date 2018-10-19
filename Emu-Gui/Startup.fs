5ee3f08a5259855e12e6376e2a57b09257c54e1c
Christian Hochlin <christian.hochlin@makingwaves.com>
2018-10-19 15:29:19 +0200

    Tried electron.net with Fsharp, moved to csharp to check for differences in init script

--- Emu-Gui/Startup.fs
+++ Emu-Gui/Startup.fs
@@ -15,7 +15,10 @@ type Startup private () =
     new (configuration: IConfiguration) as this =
         Startup() then
         this.Configuration <- configuration
-
+    member this.StartElectron =
+        async {
+            do! (Electron.WindowManager.CreateWindowAsync ()) |> Async.AwaitTask |> Async.Ignore
+        }
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
@@ -24,5 +27,6 @@ type Startup private () =
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
         app.UseMvc() |> ignore
+        Async.Start(this.StartElectron)
 
     member val Configuration : IConfiguration = null with get, set
